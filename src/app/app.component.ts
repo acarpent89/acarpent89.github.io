3fc4235302813024b089df1345caf65162f5107a
Alex Carpenter <acarpent89@gmail.com>
2018-07-28 16:20:13 -0700

    changes to add images and text to pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Alex Carpenter\'s Site';
+  meUrls =["../assets/images/CarpetDogs.jpeg","../assets/images/Council.jpeg","../assets/images/Kashi2.jpg",
+  "../assets/images/Kashi.jpg","../assets/images/Lexi.JPG"
+  ,"../assets/images/Rearview.jpeg","../assets/images/Nelson.jpg","../assets/images/Nelson2.jpg"
+  ,"../assets/images/Nelson3.jpg"];
 }
